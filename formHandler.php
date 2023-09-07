8c865709033c1f90f61eaa954d34f4c1812f7ea0
Samridhi Verma <samridhiverma01@gmail.com>
2023-09-07 19:09:51 +0530

    initial commit

---
+++ formHandler.php
@@ -0,0 +1,22 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = 'info@easy.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name: $name.\n" .
+              "User Email: $visitor_email.\n" .
+              "Subject: $subject.\n" .
+              "User Message: $message.\n";  // Removed the extra period at the end
+
+$to = "samridhiverma@gmail.com";
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";  // Used .= to append headers correctly
+
+mail($to, $email_subject, $email_body, $headers);
+header("Location: contact.html");
+?>
